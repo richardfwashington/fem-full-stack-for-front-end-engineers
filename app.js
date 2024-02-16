479005f9ecb5cfe7224d84d4d157e8e1d7c0f5ab
Richard Washington <richard.washington@taxassist.co.uk>
2024-02-16 00:02:27 +0000

    :memo: Update text

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer(function(req, res) {
-   res.write("On the way to being an automated full stack engineer!");
+   res.write("On the way to being an automated full stack engineer! This time with process manager");
    res.end();	
 }).listen(3000);
 
