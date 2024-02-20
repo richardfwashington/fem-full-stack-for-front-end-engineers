e02aef77d5bd9f5889f4025409fff825a24e073d
Richard Washington <richard.washington@taxassist.co.uk>
2024-02-20 22:24:24 +0000

    ✨ Add simple websocket server

--- index-ws.js
+++ index-ws.js
@@ -10,3 +10,30 @@ app.get('/', function(req, res) {
 server.on('request', app);
 
 server.listen(PORT, function () { console.log('Listening on ' + PORT); });
+
+/** Begin websocket */
+
+const WebsocketServer = require('ws').Server;
+
+const wss = new WebsocketServer({ server: server });
+
+wss.on('connection', function connection(ws) {
+    const numCLients = wss.clients.size;
+    console.log(`Connected clients: ${numCLients}`);
+
+    wss.broadcast(`Current visitors: ${numCLients}`);   
+    
+    if(ws.readyState === ws.OPEN) {
+        ws.send('Welcome to my server');
+    }
+
+    ws.on('close', function(){
+        console.log('A client has disconnected');
+    });
+});
+
+wss.broadcast = function broadcast(data) {
+    wss.clients.forEach(function each(client) {
+        client.send(data);
+    }); 
+}
